992fada0104cbca50f97dd518ae5f2a7c0222705
BuildTools <unconfigured@null.spigotmc.org>
2020-04-24 21:26:01 +0200

    Some major changes.

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 class homo {
 	public static void main(String[] args) {
-		System.out.println("Big homo test");
+		System.out.println("Big homo test2");
+
+		// hello guys just made a few changes here.
 	}
 }
