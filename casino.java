202e2b310085ded2f4767608d99fdd128a3caee7
BuildTools <unconfigured@null.spigotmc.org>
2020-05-01 20:15:07 +0200

    new stuff

--- casino.java
+++ casino.java
@@ -8,9 +8,18 @@ class Casino {
 	private Player player = new Player();
 	private SoundGenerator midi = new SoundGenerator();
 
+	private JFrame frame;
+
+
+
+	private boolean isRunning = false;
+
 	private JTextField one;
 	private JTextField two;
 	private JTextField three;
+
+	private JPanel centerPanel;
+	private Color themeColor;
 
 	private JTextField[] bars = new JTextField[3];
 
@@ -26,9 +35,9 @@ class Casino {
 		Font bigFont = new Font("arial", Font.BOLD, 35);
 		Font BiggerFont = new Font("arial", Font.BOLD, 90);
 
-		Color themeColor = new Color(200,20,70);
+		themeColor = new Color(200,20,70);
 
-		JFrame frame = new JFrame("Slots");
+		frame = new JFrame("Slots");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JMenuBar menuBar = new JMenuBar();
@@ -60,8 +69,8 @@ class Casino {
 		bottomPanel.add(spinButton);
 		frame.getContentPane().add(BorderLayout.SOUTH, bottomPanel);
 
-		JPanel centerPanel = new JPanel();
-		centerPanel.setBackground(new Color(20, 50, 200));
+		centerPanel = new JPanel();
+		centerPanel.setBackground(Color.blue);
 		for(int i = 0; i < 3; i++) {
 		  bars[i] = new JTextField(1);
 			bars[i].setFont(BiggerFont);
@@ -92,31 +101,57 @@ class Casino {
 		int[] values = new int[3];
 
 		for(int i = 0; i < 3; i++) {
-			int ranNum = (int) (Math.random() * 10);
+			int ranNum = (int) (Math.random() * 2);
 			values[i] = ranNum;
 			_bars[i].setText(Integer.toString(ranNum));
 		}
 
 		boolean win = values[0] == values[1] && values[2] == values[0];
-		System.out.println(values[0] + " " + values[1] + " " + values[2]);
+		//System.out.println(values[0] + " " + values[1] + " " + values[2]);
 
 		if(win) {
+			Thread winThread = new Thread(new homo());
+			winThread.start();
 			System.out.println("WIN!");
-			player.manageBalance(80, true);
 		} else {
 			System.out.println("No win.");
+			isRunning = false;
 		}
+	}
+
+	class homo implements Runnable {
+		public void run() {
+			onWin();
+		}
+	}
+
+	private void onWin() {
+		player.manageBalance(80, true);
+		for(int i = 0; i < 3; i++) {
+			midi.playSound(10,100, 1);
+		  centerPanel.setBackground(Color.yellow);
+			try {
+			 	Thread.sleep(150);
+			} catch (Exception ex) {ex.printStackTrace(); }
+			centerPanel.setBackground(Color.blue);
+		}
+		isRunning = false;
 	}
 
 	class SpinButtonActionListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
-			if(player.getBalance() >= 5) {
-				midi.playSound(30,120);
+			if(!isRunning && player.getBalance() >= 5) {
+				isRunning = true;
+				midi.playSound(30,120, 1);
 				player.manageBalance(5, false);
 				spin(bars);
-			} else {
-				midi.playSound(30,20);
+			}
+			else if (player.getBalance() < 5){
+				midi.playSound(30,20, 1);
 				System.out.println("Not enough monei");
+			} else  {
+				System.out.println("Already running.");
+				System.out.println(isRunning);
 			}
 		}
 	}
@@ -154,7 +189,7 @@ class Casino {
 
 }
 
-class Player implements Serializable {
+	class Player implements Serializable {
 
 	private double balance = 100f;
 
@@ -174,8 +209,9 @@ class Player implements Serializable {
 
 	class SoundGenerator {
 
-		public void playSound(int _instrument, int _note) {
+		public void playSound(int _instrument, int _note, int burst) {
 			try {
+				for(int i = 0; i < burst; i++) {
 				Sequencer player = MidiSystem.getSequencer();
 				player.open();
 				Sequence seq = new Sequence(Sequence.PPQ, 4);
@@ -183,23 +219,30 @@ class Player implements Serializable {
 
 				MidiEvent event = null;
 
+
 				ShortMessage first = new ShortMessage();
 				first.setMessage(192, 1, _instrument, 0);
 				MidiEvent changeInstrument = new MidiEvent(first, 1);
 				track.add(changeInstrument);
 
-				ShortMessage a = new ShortMessage();
-				a.setMessage(144, 1, _note, 100);
-				MidiEvent NoteOn = new MidiEvent(a, 1);
-				track.add(NoteOn);
 
-				ShortMessage b = new ShortMessage();
-				b.setMessage(128, 1, _note, 100);
-				MidiEvent NoteOff = new MidiEvent(b, 2);
-				track.add(NoteOff);
-				player.setSequence(seq);
-				player.start();
+					ShortMessage a = new ShortMessage();
+					a.setMessage(144, 1, _note, 100);
+					MidiEvent NoteOn = new MidiEvent(a, 1);
+					track.add(NoteOn);
 
+					ShortMessage b = new ShortMessage();
+					b.setMessage(128, 1, _note, 100);
+					MidiEvent NoteOff = new MidiEvent(b, 2);
+					track.add(NoteOff);
+					player.setSequence(seq);
+					player.start();
+					if(burst > 1) {
+						try {
+						 	Thread.sleep(120);
+						} catch (Exception ex) {ex.printStackTrace(); }
+					}
+				}
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
